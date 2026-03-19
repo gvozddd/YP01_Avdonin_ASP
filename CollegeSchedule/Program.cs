a66feb041b69b9d19f348082a6bc15b3159e62d8
gvozddd <d.avdonin54@gmail.com>
2026-03-19 12:17:01 +0700

    Подготовка DTO и реализация интерфейса

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -6,11 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 DotNetEnv.Env.Load();
 var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
- $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
- $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
- $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
-
-$"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+    $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+    $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+    $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+    $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 builder.Services.AddDbContext<AppDbContext>(options =>
  options.UseNpgsql(connectionString));
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
